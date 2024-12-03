ff203e8124a6a43e22c755689c8d293ab262f6e3
ramyapatchala <rpatchal@syr.edu>
2024-12-03 13:47:22 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,40 +1,38 @@
-import utils
 import streamlit as st
-from streaming import StreamHandler
+from langchain.chains.summarize import load_summarize_chain
+from langchain_openai import ChatOpenAI
+from langchain_community.docstore.document import Document
+from langchain_text_splitters import CharacterTextSplitter
 
-from langchain.chains import ConversationChain
+# Set API keys from session state
+openai_api_key = st.secrets['openai_api_key']
 
-st.set_page_config(page_title="Chatbot", page_icon="💬")
-st.header('Basic Chatbot')
-st.write('Allows users to interact with the LLM')
-st.write('[![view source code ](https://img.shields.io/badge/view_source_code-gray?logo=github)](https://github.com/shashankdeshpande/langchain-chatbot/blob/master/pages/1_%F0%9F%92%AC_basic_chatbot.py)')
+# Streamlit app
+st.subheader('Text Summary')
+source_text = st.text_area("Enter Source Text", height=200)
 
-class BasicChatbot:
+# If the 'Summarize' button is clicked
+if st.button("Summarize"):
+    # Validate inputs
+    if not openai_api_key:
+        st.error("Please provide the missing API keys in Settings.")
+    elif not source_text.strip():
+        st.error("Please provide the source text.")
+    else:
+        try:
+            with st.spinner('Please wait...'):
+              # Split the source text
+              text_splitter = CharacterTextSplitter()
+              texts = text_splitter.split_text(source_text)
 
-    def __init__(self):
-        utils.sync_st_session()
-        self.llm = utils.configure_llm()
-    
-    def setup_chain(self):
-        chain = ConversationChain(llm=self.llm, verbose=False)
-        return chain
-    
-    @utils.enable_chat_history
-    def main(self):
-        chain = self.setup_chain()
-        user_query = st.chat_input(placeholder="Ask me anything!")
-        if user_query:
-            utils.display_msg(user_query, 'user')
-            with st.chat_message("assistant"):
-                st_cb = StreamHandler(st.empty())
-                result = chain.invoke(
-                    {"input":user_query},
-                    {"callbacks": [st_cb]}
-                )
-                response = result["response"]
-                st.session_state.messages.append({"role": "assistant", "content": response})
-                utils.print_qa(BasicChatbot, user_query, response)
+              # Create Document objects for the texts (max 3 pages)
+              docs = [Document(page_content=t) for t in texts[:3]]
 
-if __name__ == "__main__":
-    obj = BasicChatbot()
-    obj.main()
+              # Initialize the OpenAI module, load and run the summarize chain
+              llm = ChatOpenAI(temperature=0, openai_api_key=openai_api_key)
+              chain = load_summarize_chain(llm, chain_type="map_reduce")
+              summary = chain.run(docs)
+
+              st.success(summary)
+        except Exception as e:
+            st.exception(f"An error occurred: {e}")
