5e1ca216366429088bd1959177994f5b04eb77ea
ramyapatchala <rpatchal@syr.edu>
2024-12-04 22:08:08 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,7 +12,7 @@ page_bg_img = '''
 <style>
 
 [data-testid="stAppViewContainer"] {
-background-image: url("https://raw.githubusercontent.com/ramyapatchala/langchain/main/bkgimg.jpg");
+background-image: url("https://raw.githubusercontent.com/ramyapatchala/langchain/main/bkgimg.jpeg");
 background-size: cover;
 }
 .text-box {
